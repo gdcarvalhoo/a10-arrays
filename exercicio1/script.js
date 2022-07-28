db0008cfaacc8aba229971338a0312ee360c984d
gdcarvalhoo <diascarvalhoguilherme@gmail.com>
2022-07-27 21:45:51 -0300

    Minha resolução dos exercicios - A10

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+// # Exercício 1
+
+// No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+// - O segundo array deve possuir apenas **strings**;
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+
+const arrayUm = [1, 2, 5, 7, 9, 0]
+const arrayDois = ["Aatrox","Ashe","Amumu","A.Sol","Nidalinda"]
+const arrayTres = [1,2,5,8,7,"nidalinda","batatinha","feijoada",true,false]
+
+console.log(arrayUm)
+console.log(arrayDois)
+console.log(arrayTres)
